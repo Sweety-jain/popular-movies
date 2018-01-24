1a8aad0b47d6034c512aa13851a5087637819576
namit jain <namit.k.jain@gmail.com>
2018-01-24 07:15:25 +0530

    basic code infrastructure ready

---
+++ app/src/main/java/com/example/android/popularmovies/DetailActivity.java
@@ -0,0 +1,41 @@
+package com.example.android.popularmovies;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        Intent myIntent = getIntent();
+        Bundle data = getIntent().getExtras();
+        Movie movie = (Movie) data.getParcelable("movie");
+        String title = movie.getTitle();
+        String overview = movie.getOverview();
+        String releaseDate = movie.getReleaseDate();
+        Double voteAvg = movie.getVoteAverage();
+        String vote = voteAvg.toString();
+        String imageURL = movie.getImageUrl();
+        TextView tv = (TextView)findViewById(R.id.titleTV);
+        TextView overviewTV = (TextView)findViewById(R.id.synopsisTV);
+        TextView releaseDateTV = (TextView)findViewById(R.id.releasedateTV);
+        TextView voteTextView = (TextView)findViewById(R.id.voteTV);
+        ImageView image = (ImageView)findViewById(R.id.imageView);
+
+        tv.setText(title);
+        overviewTV.setText(overview);
+        releaseDateTV.setText(releaseDate);
+        voteTextView.setText(vote);
+        Picasso.with(this)
+                .load(imageURL)
+                .into(image);
+
+    }
+}
---
+++ app/src/main/java/com/example/android/popularmovies/Movie.java
@@ -0,0 +1,72 @@
+package com.example.android.popularmovies;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Created by sweety on 1/22/2018.
+ */
+
+public class Movie implements Parcelable {
+
+    private String movieTitle;
+    private String imageUrl;
+    private String releaseDate;
+    private Double voteAverage;
+    private String plotSynopsis;
+public Movie(String mTitle,String iUrl,String rDate,Double vAverage,String pSynopsis){
+    this.movieTitle = mTitle;
+    this.imageUrl = iUrl;
+    this.releaseDate = rDate;
+    this.voteAverage = vAverage;
+    this.plotSynopsis = pSynopsis;
+}
+    protected Movie(Parcel in) {
+    this.movieTitle = in.readString();
+    this.imageUrl = in.readString();
+    this.releaseDate = in.readString();
+    this.voteAverage = in.readDouble();
+    this.plotSynopsis = in.readString();
+    }
+    public String getTitle(){
+        return movieTitle;
+    }
+    public String getOverview(){
+        return plotSynopsis;
+    }
+    public String getImageUrl(){
+        return imageUrl;
+    }
+    public String getReleaseDate(){
+        return releaseDate;
+    }
+    public Double getVoteAverage(){
+        return voteAverage;
+    }
+
+    public static final Creator<Movie> CREATOR = new Creator<Movie>() {
+        @Override
+        public Movie createFromParcel(Parcel in) {
+            return new Movie(in);
+        }
+
+        @Override
+        public Movie[] newArray(int size) {
+            return new Movie[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(movieTitle);
+        parcel.writeString(imageUrl);
+        parcel.writeString(releaseDate);
+        parcel.writeDouble(voteAverage);
+        parcel.writeString(plotSynopsis);
+    }
+}
